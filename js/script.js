52399a7c33a27d38b5f4b43169a78d9e73f42232
ilahyani <lahyani799@gmail.com>
2023-05-02 09:09:28 +0100

    reformating code

--- js/script.js
+++ js/script.js
@@ -6,83 +6,21 @@ const loadingDiv = document.querySelector(".info-bar")
 const Letter = document.querySelectorAll(".letter")
 const attempsLimit = 6, wordLength = 5
 
-
-function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter)
-}
-
-function colorLetters(idx) {
-    let pos = idx - 4
-    let tmpDailyWord = dailyWord
-    let tmpWord = word
-    for (let i = 0; i < wordLength; i++) {
-        Letter[pos].style.color = 'white'
-        if (tmpDailyWord.indexOf(tmpWord[i]) !== -1) {
-            if (tmpDailyWord[i] === tmpWord[i])
-            Letter[pos].style.backgroundColor = letterMatch
-            else { 
-                if (tmpWord.toString().substring(0, i).indexOf(tmpWord[i]) === -1)
-                Letter[pos].style.backgroundColor = letterDoesntMatch
-                else
-                Letter[pos].style.backgroundColor = letterDoesntExist
-                }
-                tmpDailyWord = tmpDailyWord.toString().replace(tmpWord[i], '.')
-                tmpWord = tmpWord.toString().replace(tmpWord[i], '.')
-            }
-            else
-            Letter[pos].style.backgroundColor = letterDoesntExist
-            pos++
-        }
-    }
-    
-    async function getDailyWord() {
-        const response = await fetch(wordAPI)
-        const wordObject = await response.json()
-        dailyWord = wordObject.word.toUpperCase()
-    }
-    
-    async function checkWord() {
-        loadingDiv.classList.remove('hidden')
-        let response = await fetch(validatewordAPI, {
-            method: 'POST',
-            body: JSON.stringify({ word: word })
-        })
-        let result = await response.json() // {"word": "crane", "validWord": true}
-        loadingDiv.classList.add('hidden')
-        //loadingDiv
-        if (result.validWord) {
-            if (word === dailyWord) {
-                alert("M9WED KHOUYA") ///////////// add play again button + change the dialogue
-                gameOver = true
-            }
-            colorLetters(idx - 1, Letter)
-        word = ""
-        if (++attempts == attempsLimit && gameOver == false) {
-            alert(dailyWord + " al7mar") ///////////// add play again button + change the dialogue
-            wordComplete = false
-            gameOver = true
-        }
-        wordComplete = false
-    }
-    else
-    alert("Not in word list. Try again")
-}
-
 getDailyWord()
 loadingDiv.classList.add('hidden')
 document.addEventListener('keydown', function (event) {
     if (!gameOver) {
         if (wordComplete) {
             if (event.key == "Enter")
-            checkWord()
+                checkWord()
             else if (event.key == "Backspace") {
                 if (idx)
                     idx--
-                    Letter[idx].innerText = ""
-                    word = word.toString().substring(0, word.length - 1)
-                    wordComplete = false
-                }
-                else
+                Letter[idx].innerText = ""
+                word = word.toString().substring(0, word.length - 1)
+                wordComplete = false
+            }
+            else
                 event.preventDefault();
         }
         else {
@@ -105,3 +43,64 @@ document.addEventListener('keydown', function (event) {
         }
     }
 })
+
+async function getDailyWord() {
+    const response = await fetch(wordAPI)
+    const wordObject = await response.json()
+    dailyWord = wordObject.word.toUpperCase()
+}
+
+async function checkWord() {
+    loadingDiv.classList.remove('hidden')
+    let response = await fetch(validatewordAPI, {
+        method: 'POST',
+        body: JSON.stringify({ word: word })
+    })
+    let result = await response.json() // {"word": "crane", "validWord": true}
+    loadingDiv.classList.add('hidden')
+    //loadingDiv
+    if (result.validWord) {
+        if (word === dailyWord) {
+            alert("M9WED KHOUYA") ///////////// add play again button + change the dialogue
+            gameOver = true
+        }
+        colorLetters(idx - 1, Letter)
+        word = ""
+        if (++attempts == attempsLimit && gameOver == false) {
+            alert(dailyWord + " al7mar") ///////////// add play again button + change the dialogue
+            wordComplete = false
+            gameOver = true
+        }
+        wordComplete = false
+    }
+    else
+        alert("Not in word list. Try again")
+}
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter)
+}
+
+function colorLetters(idx) {
+    let pos = idx - 4
+    let tmpDailyWord = dailyWord
+    let tmpWord = word
+    for (let i = 0; i < wordLength; i++) {
+        Letter[pos].style.color = 'white'
+        if (tmpDailyWord.indexOf(tmpWord[i]) !== -1) {
+            if (tmpDailyWord[i] === tmpWord[i])
+                Letter[pos].style.backgroundColor = letterMatch
+            else {
+                if (tmpWord.toString().substring(0, i).indexOf(tmpWord[i]) === -1)
+                    Letter[pos].style.backgroundColor = letterDoesntMatch
+                else
+                    Letter[pos].style.backgroundColor = letterDoesntExist
+            }
+            tmpDailyWord = tmpDailyWord.toString().replace(tmpWord[i], '.')
+            tmpWord = tmpWord.toString().replace(tmpWord[i], '.')
+        }
+        else
+            Letter[pos].style.backgroundColor = letterDoesntExist
+        pos++
+    }
+}
